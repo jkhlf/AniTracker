b26a5140c25d415cecfdc8062425e0ecd3e8d8a8
João <joao.khalaf@gmail.com>
2025-06-08 01:54:55 -0300

    first commit

---
+++ src/test/java/br/com/animetracker/AniTracker/AniTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.animetracker.AniTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
