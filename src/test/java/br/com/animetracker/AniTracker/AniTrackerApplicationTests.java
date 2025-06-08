a886f3187b4a9dd4143418797f60a96e401a8be6
João <joao.khalaf@gmail.com>
2025-06-08 18:08:29 -0300

    deploy pro render

--- src/test/java/br/com/animetracker/AniTracker/AniTrackerApplicationTests.java
+++ src/test/java/br/com/animetracker/AniTracker/AniTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.animetracker.AniTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AniTrackerApplicationTests {
 
 	@Test
